848428c980067db5335855f10fa45d23d051398b
Taito Horiuchi <taito.horiuchi@gmail.com>
2013-01-11 07:19:42 +0200

    Release 0.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='ll.theme',
-    version='0.2',
+    version='0.3',
     description="LL site theme.",
     long_description=open("README.rst").read(),
     classifiers=[
