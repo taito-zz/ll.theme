ea4891315252fa62fe549f84492d1574d87ef894
Taito Horiuchi <taito.horiuchi@gmail.com>
2013-02-12 10:28:27 +0200

    Releasa 0.5

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='ll.theme',
-    version='0.4',
+    version='0.5',
     description="LL site theme.",
     long_description=open("README.rst").read(),
     classifiers=[
