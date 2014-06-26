fc98199a087b1dd88913e1a68be4e4cae2afef5b
Taito Horiuchi <taito.horiuchi@gmail.com>
2014-06-26 22:21:04 +0300

    release 0.7

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='ll.theme',
-    version='0.6',
+    version='0.7',
     description="LL site theme.",
     long_description=open("README.rst").read(),
     classifiers=[
